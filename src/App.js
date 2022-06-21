4c15835bd89385e54745dc05554eeec3b8671d68
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-21 17:29:48 +0530

    Create NewExpense form w/o functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Expenses from "./components/Expenses/Expenses.js";
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -19,7 +20,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "./ExpenseForm.css";
+
+function ExpenseForm() {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2022-12-31" />
+        </div>
+      </div>
+      <button className="new-expense__actions" type="submit">
+        Add Expense
+      </button>
+    </form>
+  );
+}
+
+export default ExpenseForm;
