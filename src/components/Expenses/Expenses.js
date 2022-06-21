34a1443bc647a8e623504841d1ac13ee78917ece
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-21 20:25:32 +0530

    Turn ExpensesFilter into an controlled component inside Expenses Parent

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses({ expenses }) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        expenseTitle={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        expenseTitle={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseItem
+          expenseTitle={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        />
+        <ExpenseItem
+          expenseTitle={expenses[1].title}
+          amount={expenses[1].amount}
+          date={expenses[1].date}
+        />
+      </Card>
+    </div>
   );
 }
 
