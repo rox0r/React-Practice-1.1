4e9e390d3f3529984bd9934f6acc680e1a67fa9c
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-21 17:06:20 +0530

    use useState hook to change title when button is clicked

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem({ title, amount, date }) {
+function ExpenseItem({ expenseTitle, amount, date }) {
+  const [title, setTitle] = useState(expenseTitle);
+
+  function clickHandler() {
+    setTitle("Updated");
+    console.log("Clicked");
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
@@ -11,6 +18,7 @@ function ExpenseItem({ title, amount, date }) {
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,12 +7,12 @@ function Expenses({ expenses }) {
   return (
     <Card className="expenses">
       <ExpenseItem
-        title={expenses[0].title}
+        expenseTitle={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
       />
       <ExpenseItem
-        title={expenses[1].title}
+        expenseTitle={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
       />
