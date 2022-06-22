2cc31accaeaa226fb2718de311e367f2ef567619
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-22 15:54:44 +0530

    add button to open newExpense Form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,6 +6,7 @@ function ExpenseForm({ onSaveExpenseData }) {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  const [addingExpenseStatus, setAddingExpenseStatus] = useState("not-active");
 
   function titleChangerHandler(event) {
     setEnteredTitle(event.target.value);
@@ -30,45 +31,67 @@ function ExpenseForm({ onSaveExpenseData }) {
     setEnteredAmount("");
     setEnteredDate("");
     onSaveExpenseData(expenseData);
+    deactivateAddingExpense();
   }
 
-  return (
-    <form onSubmit={submitHandler} method="post">
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            onChange={titleChangerHandler}
-            value={enteredTitle}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            onChange={amountChangerHandler}
-            value={enteredAmount}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            onChange={dateChangerHandler}
-            value={enteredDate}
-          />
-        </div>
-      </div>
-      <button className="new-expense__actions" type="submit">
-        Add Expense
+  function activateAddingExpense() {
+    setAddingExpenseStatus("active");
+  }
+  function deactivateAddingExpense() {
+    setAddingExpenseStatus("not-active");
+  }
+
+  if (addingExpenseStatus === "not-active") {
+    return (
+      <button className="new-expense__actions" onClick={activateAddingExpense}>
+        Add New Expense
       </button>
-    </form>
-  );
+    );
+  } else {
+    return (
+      <form onSubmit={submitHandler} method="post">
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input
+              type="text"
+              onChange={titleChangerHandler}
+              value={enteredTitle}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              onChange={amountChangerHandler}
+              value={enteredAmount}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2022-12-31"
+              onChange={dateChangerHandler}
+              value={enteredDate}
+            />
+          </div>
+        </div>
+        <button
+          className="new-expense__actions"
+          onClick={deactivateAddingExpense}
+        >
+          Cancel
+        </button>
+        <button className="new-expense__actions" type="submit">
+          Add Expense
+        </button>
+      </form>
+    );
+  }
 }
 
 export default ExpenseForm;
